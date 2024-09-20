1cf04aca84c4b2ad70bfe8b22f558084cd1323d9
Christopher Torrington <ctorrington1@gmail.com>
2024-09-20 22:09:49 +0100

    Add State Space ABC

---
+++ state_space_interface.py
@@ -0,0 +1,28 @@
+# TODO Docstring
+"""_summary_
+"""
+
+from abc import ABC, abstractmethod
+from environment_interface import IAction
+
+class IState(ABC):
+    @abstractmethod
+    def get_actions(self) -> list[IAction]:
+        pass
+    @abstractmethod
+    def get_value(self) -> float:
+        pass
+    @abstractmethod
+    def get_reward(self) -> float:
+        pass
+    @abstractmethod
+    def is_terminal(self) -> bool:
+        pass
+
+class IStateSpace[SI](ABC):
+    @abstractmethod
+    def get_state(self, index: SI) -> IState:
+        pass
+
+class IStateProbDist(ABC):
+    pass
