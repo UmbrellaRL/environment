e64449944392bc13933688b2a8c72c3c9b995c65
Christopher Torrington <ctorrington1@gmail.com>
2024-09-20 22:12:16 +0100

    Add Environment ABC

--- environment_interface.py
+++ environment_interface.py
@@ -1,6 +1,42 @@
+# TODO
+# determine_next_state_probability_distribution method equivalent done in config tile -> injected into environment factory.
 
+from abc import ABC, abstractmethod
+from enum import Enum
 
-from abc import ABC
+from environment.state_space_interface import IState
 
-class IEnvironment(ABC):
+class IActions(ABC, Enum):
+    pass
+
+class IAction(ABC):
+    """Abstract Base Class for individual Action within the environment.
+
+    Args:
+        ABC (_type_): _description_
+    """
+
+class IEnvironment[SI](ABC):
+
+    @abstractmethod
+    def step(self, action: IAction) -> ():
+        pass
+
+    @abstractmethod
+    def render(self) -> None:
+        pass
+
+    @abstractmethod
+    def get_state(self, state_index: SI) -> IState:
+        pass
+
+    @abstractmethod
+    def get_next_states(self, state_index: SI, action: IAction) -> list[SI]:
+        pass
+
+    @abstractmethod
+    def get_transition_probability(self, state_index: SI, action: A, next_state_index: SI) -> float:
+        pass
+
+class IStateTransGraph(ABC):
     pass
