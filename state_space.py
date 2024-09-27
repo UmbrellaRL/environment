c3598780ac2a83c0bc15f8e1ec8de20518ee1ae5
Christopher Torrington <ctorrington1@gmail.com>
2024-09-27 20:24:02 +0100

    Add doc strg 👨‍⚕️

--- state_space.py
+++ state_space.py
@@ -1,30 +1,20 @@
-# TODO Docstring
-"""_summary_
-"""
+"""State Space module for class relating to State Space & States."""
 from dataclasses import dataclass
 
 from state_space_interface import IStateSpace, IState
 from environment_interface import IAction
 
 class StateSpace[SI](IStateSpace[SI]):
-    # TODO Docstring
-    """_summary_
-    """
+    """State Space is a collection made up of State objects."""
     def __init__(self, state_space: dict[SI, IState]) -> None:
         self._state_space: dict[SI, IState] = state_space
 
     def get_state(self, index: SI) -> IState: return self._state_space[index]
-    
     def number_of_states(self) -> int: return len(self._state_space)
 
 @dataclass
 class State(IState):
-    # TODO Docstring
-    """_summary_
-
-    Args:
-        IState (_type_): _description_
-    """
+    """State is the smallest within an environment."""
     actions: list[IAction]
     value: float
     reward: float
