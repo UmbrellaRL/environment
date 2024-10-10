f37e30fa7a7db6c979f512996a1ace24fe97995c
Christopher Torrington <ctorrington1@gmail.com>
2024-10-10 12:03:40 +0100

    Add API modules with examples 🛜

---
+++ api/dependencies.py
@@ -0,0 +1,13 @@
+"""Use for 3rd party service dependencies."""
+
+# from fastapi import Depends
+
+# Dep Inj example
+# # Example: Dependency for a database connection
+# def get_db():
+#     # Connect to the database here (e.g., with SQLAlchemy)
+#     db = "mock_database_connection"
+#     try:
+#         yield db
+#     finally:
+#         pass  # Close the DB connection
---
+++ api/graphql/mutations.py
@@ -0,0 +1,9 @@
+"""QraphQL mutations."""
+
+import strawberry
+# from .resolvers import create_user
+
+# Mutation example
+# @strawberry.type
+# class Mutation:
+#     create_user: str = strawberry.mutation(resolver=create_user)
