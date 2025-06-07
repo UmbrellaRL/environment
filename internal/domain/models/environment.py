317a250f7bfb7a66815011cdfb49cb0afa61c1de
Christopher Torrington <ctorrington1@gmail.com>
2025-06-07 14:11:29 +0000

    Add Environment model and update Transition model to include alias for id field

--- internal/domain/models/environment.py
+++ internal/domain/models/environment.py
@@ -2,12 +2,13 @@ from pydantic import BaseModel, Field
 
 from internal.domain.models.state import State
 from internal.domain.models.action import Action
+from internal.domain.models.transition import Transition
 
 import uuid
 
-class Transition(BaseModel):
+class Environment(BaseModel):
     id_: str = Field(alias="id", default_factory=lambda: str(uuid.uuid4()))
-    from_: State = Field(alias="from")
-    to: State = Field()
-    action: Action = Field()
-    probability: float = Field(ge=0.0, le=1.0)
+    name: str
+    states: list[State]
+    actions: list[Action]
+    transition: list[Transition]
